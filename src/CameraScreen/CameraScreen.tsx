7705adbe80a6a85c5b2e6ef159d016d2ed447405
eladattias1996 <eladattias1996@gmail.com>
2024-02-03 18:48:15 +0200

    added frame processor

--- src/CameraScreen/CameraScreen.tsx
+++ src/CameraScreen/CameraScreen.tsx
@@ -1,14 +1,29 @@
 import React, {FC} from 'react';
-import {Camera, useCameraDevice} from 'react-native-vision-camera';
+import {
+  Camera,
+  useCameraDevice,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 import {DeviceNotFound} from './components/DeviceNotFound';
 import {StyleSheet} from 'react-native';
 
 export const CameraScreen: FC = () => {
   const device = useCameraDevice('front');
 
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+  }, []);
+
   if (!device) return <DeviceNotFound />;
 
-  return <Camera isActive style={styles.fullScreen} device={device} />;
+  return (
+    <Camera
+      isActive
+      style={styles.fullScreen}
+      device={device}
+      frameProcessor={frameProcessor}
+    />
+  );
 };
 
 export const styles = StyleSheet.create({
